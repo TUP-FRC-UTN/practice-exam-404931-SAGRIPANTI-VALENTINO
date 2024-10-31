026fce9a3bb44d7339cfe67fe298e00e9f2f35f0
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-10-31 18:52:05 -0300

    despues corrijo

--- src/app/view-orders/view-orders.component.ts
+++ src/app/view-orders/view-orders.component.ts
@@ -2,11 +2,13 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ApiService } from '../service/api.service';
 import { Order } from '../models/order';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-view-orders',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, RouterLink, RouterModule, ReactiveFormsModule],
   templateUrl: './view-orders.component.html',
   styleUrl: './view-orders.component.css'
 })
@@ -15,16 +17,28 @@ export class ViewOrdersComponent implements OnInit{
   serviceOrder : ApiService = inject(ApiService)
 
   allOrders : Order[] = []
+  filteredData : Order[] = []
 
-  search : string | undefined
+  search : FormControl = new FormControl('')
 
   ngOnInit(): void {
+    this.getOrders()
+  }
+
+  getOrders() {
+    console.log("hola");
+    
     this.serviceOrder.getOrders().subscribe(data => {
       this.allOrders = data
     })
-  }
-
-  onChanges() {
-
+    this.search.valueChanges.subscribe(data => {
+      if(data === null || data === '') {
+        return this.getOrders()
+      }
+      this.allOrders = this.allOrders.filter(order => {
+        order.customerName.toUpperCase().includes(data.toUpperCase()) || order.email.toUpperCase().includes(data.toUpperCase())
+      })
+    })
+    console.log(this.allOrders);
   }
 }
