b938353e406c6ce757f3002ea96272885e352fe9
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-10-31 14:49:03 -0300

    navbar y routerlinks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { CreateOrderComponent } from './create-order/create-order.component';
+import { ViewOrdersComponent } from './view-orders/view-orders.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : "create/order", component: CreateOrderComponent
+    },
+    {
+        path : "orders", component: ViewOrdersComponent
+    },
+    {
+        path : '', component: CreateOrderComponent
+    },
+    {
+        path : "**", component: CreateOrderComponent
+    }
+];
