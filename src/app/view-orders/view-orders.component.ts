fb242887991f2ab7b0738bd808efab5b7e59acac
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-10-31 18:18:24 -0300

    lista de ordenes

--- src/app/view-orders/view-orders.component.ts
+++ src/app/view-orders/view-orders.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { Order } from '../models/order';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-orders',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './view-orders.component.html',
   styleUrl: './view-orders.component.css'
 })
-export class ViewOrdersComponent {
+export class ViewOrdersComponent implements OnInit{
+  
+  serviceOrder : ApiService = inject(ApiService)
 
+  allOrders : Order[] = []
+
+  search : string | undefined
+
+  ngOnInit(): void {
+    this.serviceOrder.getOrders().subscribe(data => {
+      this.allOrders = data
+    })
+  }
+
+  onChanges() {
+
+  }
 }
